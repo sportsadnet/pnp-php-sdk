d3f14f8d6c1bb419bd7b8984ad50840990edf023
Anatolii <ravmailbox@gmail.com>
2023-08-30 16:17:03 +0300

    added basic AI/Game resource

---
+++ src/Resources/Ai/Game.php
@@ -0,0 +1,30 @@
+<?php
+
+/** @noinspection PhpUnused */
+
+namespace Pnp\Sdk\Resources\Ai;
+
+use DateTimeInterface;
+use Pnp\Sdk\Resource;
+
+/**
+ * Class Game
+ *
+ * @package Pnp\Sdk\Resources
+ *
+ * @property int $id
+ */
+class Game extends Resource
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @return string[]
+     */
+    public function getCasts(): array
+    {
+        return [
+            'id' => 'int',
+        ];
+    }
+}
